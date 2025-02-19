9735e8a065a5a0adb044d4a822ba43d9f0e33de1
duarte <tduarte@etrr.edu.ar>
2025-02-19 15:59:09 -0300

    Update Act 11.c

--- Act 11.c
+++ Act 11.c
@@ -1,15 +1,24 @@
-//Desarrolla un algoritmo que permita, dados ciertos centímetros como entrada de tipo flotante, emitir por pantalla su equivalencia en pies (enteros) y en pulgadas. 
+//Desarrolla un algoritmo que permita, dados ciertos centímetros como entrada de tipo flotante, emitir por pantalla su equivalencia en pies (enteros) y en pulgadas.
+
 #include <stdio.h>
 
 int main(){
-    float centimetros;
-    printf("bienvenido al programa conversor de centimetros a pies y pulgadas\n");
-    printf("para comenzar ingrese su valor de centimetros:");
-    scanf("&f",  &centimetros);
+    float centimetros, pies, pulgadas;
+    int decision;
     
-    int pies = (int) centimetros / 30.48;
-    float pulgadas = (centimetros/2.54);
+    printf("Bienvenido, ingrese la cantidad de centimetros que quiere convertir: \n");
+    scanf("%f", &centimetros);
+    printf("Perfecto, ahora oprima 0 si quiera convertirlos a pies o 1 si quiere convertirlos en pulgadas: \n");
+    scanf("%d", &decision);
     
-    printf("su valor en pies es de: %d \n",pies);
-    printf("su valor en pulgadas es de: %f", pulgadas);
+    pies = centimetros/30.48;
+    pulgadas =centimetros/2.48;
+    
+    if(decision == 0){
+        printf("Perfecto, aqui tiene su conversion: %f", pies);
+    }else if(decision == 1){
+        printf("Perceto, aqui tiene su conversion: %f", pulgadas);
+    }else{
+        printf("Error, eliga el 0 o el 1");
+    }
 }
